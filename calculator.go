31e70435caf14f831e3258d4729ac7a40331b475
rizalnurizalludin <91174085+rizalnurizalludin@users.noreply.github.com>
2022-06-13 15:19:39 -0600

    menambah fungsi kalkulasi

---
+++ calculator.go
@@ -0,0 +1,26 @@
+package belajar_module
+
+/**
+a is number1 as integer
+b is number2 as integer
+opr is operator as string
+return is integer and error
+*/
+func calculate(a, b int, opr string)(int, string) {
+	var result int
+	var err string
+	switch opr{
+	case "+" ,"tambah":
+		result=a+b
+	case "-", "kurang":
+		result=a-b
+	case "*", "kali":
+		result=a*b
+	case "/" , "bagi":
+		result=a/b
+	default:
+		err="error operator name"
+	}
+
+	return result, err
+}
